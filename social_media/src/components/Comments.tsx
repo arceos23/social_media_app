bfbd4124d6f2bdbc3af1c35e608ad63de8b7238b
Shane Arceo <arceos@seas.upenn.edu>
2023-04-17 17:47:32 -0400

    post page updated to include full post with comments

--- social_media/src/components/Comments.tsx
+++ social_media/src/components/Comments.tsx
@@ -1,14 +1,12 @@
 import { FC } from "react";
 import Container from "@mui/material/Container";
 import Comment from "@/components/Comment";
-import { ObjectType } from "typescript";
 
 type CommentsProps = {
   postComments: Array<object>;
 };
 
 const Comments: FC<CommentsProps> = ({ postComments }) => {
-  console.log(postComments);
   let comments = postComments.map((comment) => (
     <Comment
       author={""}
--- social_media/src/pages/[postid].tsx
+++ social_media/src/pages/[postid].tsx
@@ -3,14 +3,18 @@ import { useRouter } from "next/router";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Comments from "@/components/Comments";
+import Post from "@/components/Post";
 import data from "@/mock/data.json";
 
-const Post = () => {
+const PostPage = () => {
   const router = useRouter();
   const { postid } = router.query;
-  let postComments = data.filter((post) => post["pid"] === postid)[0]["comments"];
+  // let postComments = data.filter((post) => post["pid"] === postid)[0]["comments"];
+  let post = data.filter((post) => post["pid"] === postid)[0];
+  let postComments = post["comments"];
   return (
     <Container>
+      <Post {...post} key={post["pid"]}></Post>
       <Comments {...{ postComments }}></Comments>
       <Link href="/">
         <Typography variant="body1" color="text.white">
@@ -21,4 +25,4 @@ const Post = () => {
   );
 };
 
-export default Post;
+export default PostPage;
