2a8229f4be6f7e8378d4e1a67cc88bf76839bf4e
Shane Arceo <arceos@seas.upenn.edu>
2023-04-19 00:13:54 -0400

    created add post form skeleton

--- social_media/src/pages/create-post.tsx
+++ social_media/src/pages/create-post.tsx
@@ -1,13 +1,45 @@
 import AuthCheck from "@/components/AuthCheck";
-import { Container } from "@mui/material";
+import Container from "@mui/material/Container";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
+import Card from "@mui/material/Card";
 
 const CreatePostPage = () => {
   return (
-    <Container>
-      <AuthCheck>
-        <h1>[TODO: insert form to create new post]</h1>
-      </AuthCheck>
-    </Container>
+    <AuthCheck>
+      <Container sx={{ background: "white" }}>
+        <Stack>
+          <Typography variant="h6" sx={{ color: "#1976d2", mt: 2 }}>
+            Add post
+          </Typography>
+        </Stack>
+        <TextField
+          autoComplete="title"
+          name="title"
+          required
+          fullWidth
+          id="title"
+          label="Title"
+          autoFocus
+          sx={{ background: "white", mt: 2, mb: 2 }}
+        ></TextField>
+        <TextField
+          autoComplete="comment"
+          name="comment"
+          required
+          fullWidth
+          id="comment"
+          label="Comment"
+          autoFocus
+          sx={{ background: "white", mb: 2 }}
+        ></TextField>
+        <Button type="submit" variant="contained" sx={{ mb: 2 }}>
+          Submit
+        </Button>
+      </Container>
+    </AuthCheck>
   );
 };
 
