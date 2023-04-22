0b18686a9587a6c59cc7ddd3d69c1e279779add9
Shane Arceo <arceos@seas.upenn.edu>
2023-04-22 18:06:48 -0400

    disabled log in button when user logged in

--- social_media/src/pages/sign-up.tsx
+++ social_media/src/pages/sign-up.tsx
@@ -3,18 +3,27 @@ import { signInWithPopup } from "firebase/auth";
 import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
 import Avatar from "@mui/material/Avatar";
+import Box from "@mui/material/Box";
 
 const SignUpPage = () => {
   return (
     <Container>
-      <Button type="submit" variant="contained" sx={{ textTransform: "none" }} onClick={signInWithGoogle}>
-        <Avatar
-          src="Google__G__Logo.svg"
-          alt="Google G logo"
-          sx={{ mr: 2, height: "1.25rem", width: "1.25rem" }}
-        ></Avatar>
-        Sign in with Google
-      </Button>
+      <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", background: "gray" }}>
+        <Button
+          type="submit"
+          variant="contained"
+          sx={{ textTransform: "none", m: 2 }}
+          onClick={signInWithGoogle}
+          disabled={auth.currentUser?.uid !== undefined}
+        >
+          <Avatar
+            src="Google__G__Logo.svg"
+            alt="Google G logo"
+            sx={{ mr: 2, height: "1.25rem", width: "1.25rem" }}
+          ></Avatar>
+          Sign in with Google
+        </Button>
+      </Box>
     </Container>
   );
 };
