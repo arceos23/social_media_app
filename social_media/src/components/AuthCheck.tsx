f639ce8844a10a9b5ef6d57aff9e3339aa43ce40
Shane Arceo <arceos@seas.upenn.edu>
2023-04-22 22:09:00 -0400

    authcheck component updated to conditionally render sign in, loading, and content

--- social_media/src/components/AuthCheck.tsx
+++ social_media/src/components/AuthCheck.tsx
@@ -1,8 +1,20 @@
 import Link from "next/link";
 import { auth } from "@/lib/firebase";
+import { Container, Typography } from "@mui/material";
 
 const AuthCheck = (props: any) => {
-  return auth.currentUser ? props.children : <Link href="sign-up">You must be signed in.</Link>;
+  return auth.currentUser ? (
+    props.children
+  ) : (
+    <Container sx={{ background: "gray" }}>
+      <Link href="sign-up">
+        <Typography variant="body1" sx={{ mt: 2, mb: 2, p: 2 }}>
+          You must be signed in.
+        </Typography>
+      </Link>
+      ;
+    </Container>
+  );
 };
 
 export default AuthCheck;
